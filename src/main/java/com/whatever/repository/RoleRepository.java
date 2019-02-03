27561a92ba8779ee7048e84619d4b51296e55cc8
Kaisum <kaisumaro445@hotmail.com>
2019-02-03 17:15:51 +0100

    Authentication boiler template added.

---
+++ src/main/java/com/whatever/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.whatever.repository;
+
+import com.whatever.model.Role;
+import com.whatever.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
